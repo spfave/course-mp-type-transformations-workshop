2108d13d78f8b7220f20e0e58be0c18e2afc49a0
Sebastian F <sebpfdev@gmail.com>
2023-09-13 23:01:22 -0400

    Complete section 3

--- src/03-template-literals/18-template-literals-in-object-keys.problem.ts
+++ src/03-template-literals/18-template-literals-in-object-keys.problem.ts
@@ -1,21 +1,21 @@
-import { Equal, Expect } from "../helpers/type-utils";
+import { Equal, Expect } from '../helpers/type-utils';
 
-type TemplateLiteralKey = `${"user" | "post" | "comment"}${"Id" | "Name"}`;
+type TemplateLiteralKey = `${'user' | 'post' | 'comment'}${'Id' | 'Name'}`;
 
-type ObjectOfKeys = unknown;
+type ObjectOfKeys = Record<TemplateLiteralKey, string>;
 
 type tests = [
-  Expect<
-    Equal<
-      ObjectOfKeys,
-      {
-        userId: string;
-        userName: string;
-        postId: string;
-        postName: string;
-        commentId: string;
-        commentName: string;
-      }
-    >
-  >,
+	Expect<
+		Equal<
+			ObjectOfKeys,
+			{
+				userId: string;
+				userName: string;
+				postId: string;
+				postName: string;
+				commentId: string;
+				commentName: string;
+			}
+		>
+	>
 ];
