85dd30e1712227eafa865f8acc762c5791968745
Sebastian F <sebpfdev@gmail.com>
2023-10-09 16:23:34 -0400

    Complete type transformation challenges

--- src/06-challenges/37-get-dynamic-path-params.problem.ts
+++ src/06-challenges/37-get-dynamic-path-params.problem.ts
@@ -1,10 +1,19 @@
 import { Equal, Expect } from "../helpers/type-utils";
+import { S } from 'ts-toolbelt';
 
 type UserPath = "/users/:id";
 
 type UserOrganisationPath = "/users/:id/organisations/:organisationId";
 
-type ExtractPathParams = unknown;
+// type ExtractPathParams = unknown;
+type ExtractPathParams<TPath extends string> = {
+  [K in S.Split<TPath, '/'>[number] as K extends `:${infer P}` 
+    ? P 
+    : never]: string;
+};
+
+type test1 = ExtractPathParams<UserPath>;
+type test2 = ExtractPathParams<UserOrganisationPath>;
 
 type tests = [
   Expect<Equal<ExtractPathParams<UserPath>, { id: string }>>,
